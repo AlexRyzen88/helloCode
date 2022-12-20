a472c4f5f2ed7ed7eadd0e9c4c1894d16bdc4e28
Alex <xath@mail.ru>
2022-12-21 00:03:06 +0300

    add masha

---
+++ EXAMPLES/example005_FL/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+ 
+if(username.ToLower() == "Маша")
+
+{
+    Console.WriteLine("ура это Маша");
+
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
